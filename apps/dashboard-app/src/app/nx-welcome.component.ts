54c97330594855738f09df5e9aa431a5a0e54f44
NiltonCadamuro <niltonjrcadamuro@gmail.com>
2025-02-23 21:30:11 -0300

    feat: material ui v16 implemented, font family imported and new lib for material ui created

--- apps/dashboard-app/src/app/nx-welcome.component.ts
+++ apps/dashboard-app/src/app/nx-welcome.component.ts
@@ -1,13 +1,17 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MaterialStandaloneModule } from '@angular-project/material';
 
 @Component({
   selector: 'app-nx-welcome',
-  imports: [CommonModule],
+  imports: [CommonModule, MaterialStandaloneModule],
   template: `
     <div class="container">
       <h1 class="text-center">Bootstrap 3 Funcionando!</h1>
       <button class="btn btn-primary">Clique aqui</button>
+      <h1>Teste Material + Bootstrap</h1>
+      <button mat-raised-button color="primary">Botão Material</button>
+      <button class="btn btn-primary">Botão Bootstrap 3</button>
     </div>
   `,
   styles: [],
---
+++ libs/shared/material/src/lib/material/material.component.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+
+@NgModule({
+  imports: [MatButtonModule, MatIconModule, MatCardModule],
+  exports: [MatButtonModule, MatIconModule, MatCardModule],
+})
+export class MaterialStandaloneModule {}
